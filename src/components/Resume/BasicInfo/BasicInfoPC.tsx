affa459b5ecec2bb694da07e2b22ce4548ecd9e6
syei <always1115ken@gmail.com>
2020-11-24 07:55:34 +0900

    フリガナ && 年齢追加

--- src/components/Resume/BasicInfo/BasicInfoPC.tsx
+++ src/components/Resume/BasicInfo/BasicInfoPC.tsx
@@ -11,6 +11,7 @@ import {
 } from "@material-ui/core";
 
 import initial from "../../../utils/initial.json";
+import ages from "../../../utils/age.json";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -29,8 +30,12 @@ const useStyles = makeStyles((theme: Theme) =>
             minWidth: "200px",
             margin: "15px",
         },
+        basicContents: {
+            aligin: "center",
+            textAlign: "center",
+        },
         basicTitle: {
-            textAlign: "left",
+            textAlign: "center",
             fontSize: "25px",
             fontWeight: 500,
         },
@@ -44,71 +49,101 @@ const useStyles = makeStyles((theme: Theme) =>
 
 function BasicInfoPC() {
     const classes = useStyles();
-    const [age, setAge] = React.useState('');
     const [fName, setFName] = React.useState('');
     const [gName, setGName] = React.useState('');
+    const [gender, setGender] = React.useState('');
+    const [age, setAge] = React.useState('');
 
-    const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-        setAge(event.target.value as string);
-    };
     const fNameChange = (event: React.ChangeEvent<{ value: unknown }>) => {
         setFName(event.target.value as string);
      };
     const gNameChange = (event: React.ChangeEvent<{ value: unknown }>) => {
         setGName(event.target.value as string);
     };
+    const genderChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+        setGender(event.target.value as string);
+    };
+    const ageChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+        setAge(event.target.value as string);
+    };
 
     return (
         <div className={classes.root}>
             <p className={classes.basicTitle}>基本情報</p>
-            <TextField
-                id="standard-multiline-flexible"
-                multiline
-                className={classes.resumeName}
-                label="氏名"
-            />
-            <FormControl className={classes.formControl}>
-                <InputLabel id="demo-simple-select-label">名</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={fName}
-                    onChange={fNameChange}
-                >
-                    {initial.map(ini => (
-                        <MenuItem value={ini.value}>{ini.initial}</MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
-
-            <FormControl className={classes.formControl}>
-            <InputLabel id="demo-simple-select-label">姓</InputLabel>
-            <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={gName}
-                    onChange={gNameChange}
-                >
-                {initial.map(ini => (
-                    <MenuItem value={ini.value}>{ini.initial}</MenuItem>
-                ))}
-                </Select>
-            </FormControl>
-
-            <FormControl className={classes.formControl}>
-                <InputLabel id="demo-simple-select-label">性別</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                        value={age}
-                    onChange={handleChange}
-                >
-                    <MenuItem value={10}>男性</MenuItem>
-                    <MenuItem value={20}>女性</MenuItem>
-                </Select>
-            </FormControl>
-            <div>
-            </div>
+            <Grid container spacing={3} className={classes.basicContents}>
+                <Grid item xs={12}>
+                    <TextField
+                        id="standard-multiline-flexible"
+                        multiline
+                        className={classes.resumeName}
+                        label="氏名"
+                    />
+                    <TextField
+                        id="standard-multiline-flexible"
+                        multiline
+                        className={classes.resumeName}
+                        label="フリガナ"
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    {/*イニシャル*/}
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="demo-simple-select-label">名</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={fName}
+                            onChange={fNameChange}
+                        >
+                            {initial.map(ini => (
+                                <MenuItem value={ini.value}>{ini.initial}</MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                    <FormControl className={classes.formControl}>
+                    <InputLabel id="demo-simple-select-label">姓</InputLabel>
+                    <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={gName}
+                            onChange={gNameChange}
+                        >
+                        {initial.map(ini => (
+                            <MenuItem value={ini.value}>{ini.initial}</MenuItem>
+                        ))}
+                        </Select>
+                    </FormControl>
+                    {/*性別*/}
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="demo-simple-select-label">性別</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={gender}
+                            onChange={genderChange}
+                        >
+                            <MenuItem value={10}>男性</MenuItem>
+                            <MenuItem value={20}>女性</MenuItem>
+                        </Select>
+                    </FormControl>
+                    {/*年齢*/}
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="demo-simple-select-label">年齢</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={age}
+                            onChange={ageChange}
+                        >
+                            {ages.map(age => (
+                                <MenuItem value={age.value}>{age.age}</MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12}>
+                </Grid>
+            </Grid>
         </div>
     );
 }
